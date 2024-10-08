dcb1b073991071af7b5d0d41a2732a2e0571602a
accawsdevops <acc.aws.devops@gmail.com>
2024-10-08 19:55:43 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,6 +3,10 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 
 echo "Starting after_install.sh script..."
 
+# Clean up existing files in /var/www/html
+echo "Cleaning up existing files in /var/www/html..."
+sudo rm -rf /var/www/html/*
+
 # Check if Apache is installed
 if ! command -v httpd &> /dev/null; then
     echo "Apache (httpd) is not installed. Installing..."
@@ -19,13 +23,13 @@ else
     echo "Apache is already running."
 fi
 
-# Change ownership of files and log any errors.
+# Change ownership of files and log any errors
 if ! sudo chown -R apache:apache /var/www/html/*; then
     echo "Failed to change ownership of files." >&2
     exit 1
 fi
 
-# Restart the web server and log any errors.
+# Restart the web server and log any errors
 if ! sudo systemctl restart httpd; then
     echo "Failed to restart Apache." >&2
     exit 1
